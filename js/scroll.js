4e8d2b01a9e64b33f260e9650aa68a614bf1fc93
SveinJH <43957538+SveinJH@users.noreply.github.com>
2019-10-13 13:10:06 +0200

    Work 13/10/19

---
+++ js/scroll.js
@@ -0,0 +1,38 @@
+const about = document.querySelector('.about__text');
+const skills = document.querySelector('.skills');
+const projects = document.querySelector('.projects');
+const contact = document.querySelector('.contact');
+
+const aboutBtn = document.querySelector('.aboutBtn');
+const skillsBtn = document.querySelector('.skillsBtn');
+const projectsBtn = document.querySelector('.projectsBtn');
+const contactBtn = document.querySelector('.contactBtn');
+const readMoreBtn = document.querySelector('.readMoreBtn');
+const contactMeBtn = document.querySelector('.contactMeBtn');
+const scrollBtn = document.querySelector('.header__icon');
+
+// Scrollers
+const scrollToAbout = () => {
+    about.scrollIntoView({ behavior: 'smooth' });
+};
+
+const scrollToSkills = () => {
+    skills.scrollIntoView({ behavior: 'smooth' });
+};
+
+const scrollToProjects = () => {
+    projects.scrollIntoView({ behavior: 'smooth' });
+};
+
+const scrollToContact = () => {
+    contact.scrollIntoView({ behavior: 'smooth' });
+};
+
+// Event listeners
+scrollBtn.addEventListener('click', scrollToAbout);
+aboutBtn.addEventListener('click', scrollToAbout);
+skillsBtn.addEventListener('click', scrollToSkills);
+projectsBtn.addEventListener('click', scrollToProjects);
+contactBtn.addEventListener('click', scrollToContact);
+readMoreBtn.addEventListener('click', scrollToAbout);
+contactMeBtn.addEventListener('click', scrollToContact);
